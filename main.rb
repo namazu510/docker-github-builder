33426fd2d5d481358b07797b385d0a69ef6720a5
namazu <namazu510@hotmail.co.jp>
2017-07-11 22:58:06 +0900

    JSON送信するように

--- main.rb
+++ main.rb
@@ -10,24 +10,37 @@ EM::WebSocket.start({host: HOST , port: PORT}) do |ws_conn|
     puts 'connected'
   end
 
-  ws_conn.onmesage do |message|
+  ws_conn.onmessage do |message|
     puts "recived message #{message}"
     params = JSON.parse(message)
-    param_str = "-r #{params.repo} -t #{params.token} -c #{params.commit_id}"
-    Open3.popen3("./image-build #{param_str}") do |i, o, e, w|
+    cmd  = "./image-build -r #{params.repo} -t #{params.token} -c #{params.commit_id}"
+
+    ws_conn.send(JSON.generate({type: "start", data: params}))
+    Open3.popen3(cmd) do |i, o, e, w|
+      i.close
       o.each do |line|
         puts line
-        ws_conn.send(line)
+        ws_conn.send(JSON.generate({type: "log", data: line}))
       end
       e.each do |line|
         puts line
-        ws_conn.send(line)
+        ws_conn.send(JSON.generate({type: "err_log", data: line}))
       end
+
+      ws_conn.send(JSON.generate({
+        type: "exit",
+        data: {
+          code: w.value.exit
+        }
+      }))
+    end
+  end
+
+  ws_conn.onclose do
+    puts "closed"
   end
 
   ws_conn.onerror do |error|
-    if error.kind_of?(EM:WebSocket::WebSocketError) do
-      p error
-    end
+    p error
   end
 end
