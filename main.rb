f58e0208107883a7ee4617b613c09937c343bd57
namazu510 <namazu510@hotmail.co.jp>
2018-02-15 02:54:21 +0900

    Fix ちゃんと動かなかったprocの呼び出しをちゃんと動くOpen3の拡張モジュールで置き換えた

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'open3'
 require 'digest/md5'
 
+require './open3_ext.rb'
 require './build_info.rb'
 
 HOST = ENV['HOST'] || '0.0.0.0'
@@ -20,50 +21,37 @@ EM::WebSocket.start({host: HOST , port: PORT}) do |ws_conn|
     ws_conn.send(JSON.generate({type: 'start', data: params}))
     build = BuildInfo.new(params)
 
-    cmd_handler = Proc.new do |i, o, e, w|
-      i.close
-      o.each do |line|
-        puts line
-        ws_conn.send(JSON.generate({type: 'log', data: line}))
-      end
-      e.each do |line|
-        puts line
-        ws_conn.send(JSON.generate({type: 'err_log', data: line}))
-      end
-      w.value.to_i
-    end
-
     # GIT CLONE
     `mkdir repo`
     `rm -rf #{build.clone_dir}`
-    status = Open3.popen3("#{build.clone_cmd} #{build.clone_dir}", &cmd_handler)
-    if status != 0
-      ws_conn.close
+    Open3.popen3_with_ws("#{build.clone_cmd} #{build.clone_dir}", ws_conn) do |status|
+      return ws_conn.close if status != 0
     end
 
     # CHECKOUT
     if build.commit_id
-      status = Open3.popen3("cd #{build.clone_dir} && git checkout #{build.commit_id}", &cmd_handler)
-      if status != 0
-        ws_conn.close
+      Open3.popen3_with_ws("cd #{build.clone_dir} && git checkout #{build.commit_id}", ws_conn) do |status|
+        return ws_conn.close if status != 0
       end
     end
 
     # BUILD
     commit_id = `cd #{build.clone_dir} && git show -s --format=%H` # ビルドするコミットID
     docker_tag = "#{DOCKER_REG}/#{build.repo_uri.downcase}:#{commit_id}"
-
-    state = Open3.popen3("cd #{build.clone_dir} && docker build . -t #{docker_tag}", &cmd_handler)
-    if state != 0
-      ws_conn.send(JSON.generate({type: 'exit', data:
-          { code: state, repo_url: docker_tag, commit_id: commit_id}}))
-      ws_conn.close
+    Open3.popen3_with_ws("cd #{build.clone_dir} && docker build . -t #{docker_tag}", ws_conn) do |state|
+      if state != 0
+        ws_conn.send(JSON.generate({type: 'exit', data:
+            { code: state, repo_url: docker_tag, commit_id: commit_id}}))
+        ws_conn.close
+        return
+      end
     end
 
     # PUSH
-    state = Open3.popen3("docker push #{docker_tag}", &cmd_handler)
-    ws_conn.send(JSON.generate({type: 'exit', data:
-        { code: state, repo_url: docker_tag, commit_id: commit_id}}))
+    Open3.popen3_with_ws("docker push #{docker_tag}", ws_conn) do |state|
+      ws_conn.send(JSON.generate({type: 'exit', data:
+          { code: state, repo_url: docker_tag, commit_id: commit_id}}))
+    end
   end
 
   ws_conn.onclose do
